5b3dd68698b76cb28786951c12090c990331823e
Jeffdude <jeffmilling@gmail.com>
2020-08-21 16:51:20 -0700

    day 6. completeset functionality

--- api.py
+++ api.py
@@ -1,9 +1,12 @@
 from IPython import embed
 import flask
 import json
+import os
 from flask_cors import CORS, cross_origin
+import flask_uploads
 import time
 from functools import wraps
+from werkzeug.utils import secure_filename
 
 import db
 
@@ -13,6 +16,13 @@ CORS(app)
 app.config["DEBUG"] = True
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['Access-Control-Allow-Origin'] = '*'
+
+images = flask_uploads.UploadSet(
+    'images',
+    flask_uploads.IMAGES,
+    default_dest=lambda app: os.path.join(app.root_path, 'images'),
+)
+flask_uploads.configure_uploads(app, images)
 
 def byte_to_string(byte_string):
     return byte_string.decode("utf-8")
@@ -66,7 +76,8 @@ def api_get_inventory(python_id=None):
     return flask.jsonify(parse_list_to_map(db.query(query_string)))
 
 def python_get_inventory(id):
-    parse_list_to_map(db.query(query_string))
+    query_string = "SELECT quantity FROM inventory WHERE id={}".format(id)
+    return int(db.query(query_string)[0][0])
 
 def python_create_inventory(id, quantity):
     query_string = (
@@ -84,7 +95,7 @@ def api_increment_inventory():
     request_id = data.get('id')
     request_quantity = data.get('quantity')
     query_string = (
-        "UPDATE inventory set quantity=quantity+{quantity} where id={id}"
+        "UPDATE inventory SET quantity=quantity+{quantity} WHERE id={id}"
     )
     if request_id and request_quantity:
         query_string = query_string.format(id=request_id, quantity=request_quantity)
@@ -100,7 +111,7 @@ def api_decrement_inventory():
     request_id = data.get('id')
     request_quantity = data.get('quantity')
     query_string = (
-        "UPDATE inventory set quantity=quantity-{quantity} where id={id}"
+        "UPDATE inventory SET quantity=quantity-{quantity} WHERE id={id}"
     )
     if request_id and request_quantity:
         query_string = query_string.format(id=request_id, quantity=request_quantity)
@@ -109,7 +120,20 @@ def api_decrement_inventory():
     else:
         return page_not_found(None)
 
-def python_get_next_UID():
+def python_decrement_inventory(id, quantity):
+    query_string = (
+        "UPDATE inventory SET quantity=quantity-{quantity} WHERE id={id}"
+    ).format(id=id, quantity=quantity)
+
+    try:
+        db.query(query_string, write=True)
+    except:
+        return False
+
+    return True
+
+
+def python_get_next_part_UID():
     query_string = "SELECT MAX(id) FROM inventory"
     return(db.query(query_string)[0][0] + 1)
 
@@ -191,7 +215,7 @@ def api_modify_part():
 @cross_origin()
 def api_create_part():
     post_data = json.loads(flask.request.data.decode('utf-8'))
-    next_id = python_get_next_UID()
+    next_id = python_get_next_part_UID()
 
     expected_data = ['name', 'type', 'active', 'color', 'created_at']
     expected_string = ", ".join(expected_data)
@@ -235,11 +259,196 @@ def api_create_part():
 
 # ==========================  COMPLETE SETS  ==================================
 
+def python_get_next_completeset_UID():
+    query_string = "SELECT MAX(id) FROM completesets"
+    result = db.query(query_string)
+    if result[0][0] is not None:
+        return result[0][0] + 1
+
+    return 0
 
 @app.route('/api/v1/completesets/fetch-all', methods=['GET'])
 @cross_origin()
 def api_all_completesets():
     return flask.jsonify(db.query("SELECT * FROM completesets"))
+
+@app.route('/api/v1/completesets/fetch', methods=['GET'])
+@cross_origin()
+def api_get_completesets(python_id=None):
+    query_string = "SELECT id, name, active, created_at FROM completesets WHERE "
+    request_id = flask.request.args.get('id', python_id)
+    if request_id:
+        query_string += "id=" + str(request_id);
+    elif flask.request.args.get('id_list'):
+        id_list = json.loads(flask.request.args.get('id_list'))
+        query_string += "id IN ({})".format(",".join(str(i) for i in id_list))
+    else:
+        return page_not_found(None)
+    return flask.jsonify(parse_list_to_map(db.query(query_string)))
+
+@app.route('/api/v1/completesets/actions/create', methods=['PUT'])
+@cross_origin()
+def api_create_completeset():
+    post_data = flask.request
+    form_data = post_data.form
+
+    next_id = python_get_next_completeset_UID()
+    
+    expected_data = [
+        'name', 'active', 'created_at',
+        'awheel1', 'awheel2', 'atruck', 'adeck', 'agrip',
+        'bwheel1', 'bwheel2', 'btruck', 'bdeck', 'bgrip',
+    ]
+    for key in expected_data:
+        if key not in post_data.form:
+            print("CREATE FAILED. MISSING: ", key)
+            return page_not_found(None)
+
+
+    if 'image_data' not in post_data.files:
+        print('CREATE FAILED. Not file data found.')
+        return page_not_found(None)
+    
+    try:
+        filename = images.save(post_data.files['image_data'])
+        print("Saved {}".format(filename))
+    except flask_uploads.UploadNotAllowed as e:
+        print('Image upload failed! Filetype rejected.' + str(e))
+        raise
+
+    expected_string = ", ".join(expected_data)
+    query_string = (
+        "INSERT INTO completesets (id, filename, " + expected_string + ") "
+        "VALUES ({id}, \"{filename}\", \"{name}\", {active}, {created_at}, "
+        "{awheel1}, {awheel2}, {atruck}, {adeck}, {agrip}, "
+        "{bwheel1}, {bwheel2}, {btruck}, {bdeck}, {bgrip})"
+    ).format(
+        id=next_id,
+        filename=filename,
+        name=form_data['name'],
+        active=0 if form_data['active'] else 1,
+        created_at=form_data['created_at'],
+
+        awheel1=form_data['awheel1'],
+        awheel2=form_data['awheel2'],
+        atruck=form_data['atruck'],
+        adeck=form_data['adeck'],
+        agrip=form_data['agrip'],
+
+        bwheel1=form_data['bwheel1'],
+        bwheel2=form_data['bwheel2'],
+        btruck=form_data['btruck'],
+        bdeck=form_data['bdeck'],
+        bgrip=form_data['bgrip'],
+    )
+
+    db.query(query_string, write=True)
+    
+    return api_get_completesets(python_id=next_id)
+
+def python_get_completeset_active(id):
+    query_string = "SELECT (active) FROM completesets WHERE id={}".format(id)
+    result = not bool(int(db.query(query_string)[0][0]))
+    return result
+
+@app.route('/api/v1/completesets/actions/modify', methods=['PUT'])
+@cross_origin()
+def api_modify_completeset():
+    data = json.loads(flask.request.data.decode('utf-8'))
+    
+    expected = ['id', 'action']
+    for key in expected:
+        if key not in data:
+            print("MODIFY FAILED. {} NOT PROVIDED".format(key))
+            return
+    queries = [] 
+    if data['action'] == 'toggle_active':
+        query_string = "UPDATE parts SET "
+        currently_active = python_get_completeset_active(data['id'])
+        if currently_active:
+            query_string += "active=1"
+        else:
+            query_string += "active=0"
+        queries.append(query_string)
+    elif data['action'] == 'delete':
+        queries = [
+            "DELETE FROM completesets",
+        ]
+
+
+    for query_string in queries:
+        query_string += " WHERE id=" + str(data['id'])
+        db.query(query_string, write=True)
+    return "True"
+
+def python_get_completeset_id_quantities(id):
+    query_string = (
+        "SELECT awheel1, awheel2, atruck, adeck, agrip, "
+        "bwheel1, bwheel2, btruck, bdeck, bgrip FROM completesets WHERE id={}".format(id)
+    )
+    result = db.query(query_string)
+    if len(result) != 1:
+        print("Received none or multiple responses. Failing.")
+        return {}
+
+    id_tuple = result[0]
+    id_quantities = {}
+    for id in id_tuple:
+        if id in id_quantities:
+            id_quantities[id] += 1
+        else:
+            id_quantities[id] = 1
+
+    return id_quantities
+
+@app.route('/api/v1/completesets/actions/withdraw', methods=['PUT'])
+@cross_origin()
+def api_withdraw_completeset():
+    data = json.loads(flask.request.data.decode('utf-8'))
+    request_id = data.get('id')
+    request_quantity = data.get('quantity')
+
+    id_quantities = python_get_completeset_id_quantities(request_id)
+    for id in id_quantities:
+        id_quantities[id] *= int(request_quantity)
+    
+    for id in id_quantities:
+        expected_quantity = python_get_inventory(id) - id_quantities[id]
+        python_decrement_inventory(id, id_quantities[id])
+        result_quantity = python_get_inventory(id) 
+        if expected_quantity != result_quantity:
+            print("ERROR ID {}: Expected quantity: {}. Result quantity: {}.".format(
+                id, expected_quantity, result_quantity)
+            )
+            success = False
+
+    return ""
+
+"""
+    actions/stock: Returns tuple: (
+        the maximum amount of completesets able to be withdrawn,
+        and the id of the limiting factor part
+    )
+"""
+@app.route('/api/v1/completesets/actions/stock', methods=['GET'])
+@cross_origin()
+def api_get_completeset_stock():
+    request_id = flask.request.args.get('id')
+
+    id_quantities = python_get_completeset_id_quantities(request_id)
+    id_inventories = {}
+    for id in id_quantities:
+        id_inventories[id] = python_get_inventory(id)
+
+    id_limits = []
+    for id in id_inventories:
+        id_limits.append((id_inventories[id] / id_quantities[id], id))
+    limit = min(id_limits)
+
+    return flask.jsonify((int(limit[0]), limit[1]))
+
+
+
 
 if __name__ == "__main__":
     python_get_part_active(1)
